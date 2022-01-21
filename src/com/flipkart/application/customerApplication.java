1bdc5d4cb5fe20d7db43f2185833b3a8a96a4e23
Kotikalapudi Venkat Karthik <venkat.karthik@BLRETV-C02FFN25.local>
2022-01-21 14:00:34 +0530

    assignment 5

--- src/com/flipkart/application/customerApplication.java
+++ src/com/flipkart/application/customerApplication.java
@@ -3,6 +3,10 @@
  */
 package com.flipkart.application;
 
+import java.util.Scanner;
+
+import com.flipkart.business.CustomerService;
+
 /**
  * @author venkat.karthik
  *
@@ -14,7 +18,69 @@ public class customerApplication {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//create instance of customer service
+		CustomerService service = new CustomerService();
+		
+		System.out.println("new - for new customer");
+		System.out.println("update - for updating");
+		System.out.println("del - delete customer");
+		System.out.println("list - list all customers");
+		System.out.println("quit - exit the session");
+		System.out.println("Session has started.............");
+		System.out.println();
+		
+		Scanner scanner = new Scanner(System.in);
 
+		while(true) {
+			String command = scanner.nextLine();
+			
+			if(command.equals("quit")) {
+				System.out.println("Exiting session..........");
+				break;
+			}
+			
+			System.out.println("Enter Name: ");
+			
+			String name = scanner.nextLine();
+			
+			switch(command) {
+			case "new":
+				
+				System.out.println("Enter Address: ");
+				String adress = scanner.nextLine();
+
+				service.createCustomer(name, adress);
+				break;
+				
+			case "update":
+
+				System.out.println("Enter changed name or enter skip");
+				String newName = scanner.nextLine();
+
+				System.out.println("Enter changed adress or enter skip");
+				String newAdress = scanner.nextLine();
+
+				service.updateCustomer(name, newName, newAdress);
+				break;
+				
+			case "del":
+				
+				service.deleteCustomer(name);
+				break;
+				
+			case "list":
+				
+				if(name.equals("admin")) {
+					service.listCustomer();
+				}
+				break;
+				
+			default:
+				System.out.println("Invalid command");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
--- src/com/flipkart/business/CustomerService.java
+++ src/com/flipkart/business/CustomerService.java
@@ -3,22 +3,50 @@
  */
 package com.flipkart.business;
 
+import java.util.ArrayList;
+
+import com.flipkart.bean.Customer;
+
 /**
  * @author venkat.karthik
  *
  */
 public class CustomerService {
 	//the service class implementing all the operations for creaating, updating and deleting the customers
-	public void createCustomer() {
+	ArrayList<Customer> myCustomers = new ArrayList<Customer>();
+	
+	public void createCustomer(String customerName, String customerAdress) {
+		Customer newCustomer = new Customer();
+		newCustomer.setCustomerId(101+myCustomers.size());
+		newCustomer.setCustomerName(customerName);
+		newCustomer.setCustomerAdress(customerAdress);
+		myCustomers.add(newCustomer);
 		System.out.println("Customer is created");
 	}
-	public void updateCustomer() {
+	
+	public void updateCustomer(String customerName, String newName, String newAdress) {
+		for(Customer customer: myCustomers) {
+			if(customer.getCustomerName().equals(customerName)) {
+				if(!newName.equals("skip")) {
+					customer.setCustomerName(newName);
+				}
+				if(!newName.equals("skip")) {
+					customer.setCustomerAdress(newAdress);
+				}
+			}
+		}
 		System.out.println("Customer is updated");
 	}
+	
 	public void listCustomer() {
-		System.out.println("Customer is listed");
+		for(Customer customer: myCustomers) {
+			System.out.println("Customer Id is:" + customer.getCustomerId() + "\n" + "Customer Name:" + customer.getCustomerName());
+			System.out.println();
+		}
 	}
-	public void deleteCustomer() {
+	
+	public void deleteCustomer(String customerName) {
+		myCustomers.removeIf(customer -> customer.getCustomerName().equals(customerName));
 		System.out.println("Customer is deleted");
 	}
 }
